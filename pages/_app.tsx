2630960f8efeb56edf46f5ba017db5b62e49ebc1
chris0610 <chrislow360@gmail.com>
2022-05-20 15:47:44 +0800

    remove geo filter context, danfojs and chartjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,11 @@ import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 
 import Layout from "../components/Layout";
-import LocationContextProvider from "../contexts/GeoFiltersContext";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <LocationContextProvider>
-        <Component {...pageProps} />
-      </LocationContextProvider>
+      <Component {...pageProps} />
     </Layout>
   );
 }
