b7e07dc6ea2d702a18e01edcfdfe1c1da1d317e5
chris0610 <chrislow360@gmail.com>
2022-05-23 19:49:33 +0800

    link logo to home page based on locale

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 import { LANG } from "../lib/constants";
@@ -7,21 +8,23 @@ import { LANG } from "../lib/constants";
 const Header = () => {
   const router = useRouter();
   const { i18n } = useTranslation();
-  const { pathname, asPath, query } = router;
+  const { pathname, asPath, query, locale, defaultLocale } = router;
 
   return (
     <div className="fixed top-0 z-40 flex h-14 w-full items-center justify-center bg-accent">
       <nav className="flex w-full max-w-screen-lg items-center justify-between p-3 text-white lg:py-3 lg:px-0">
-        <div className="flex items-center justify-center gap-4">
-          {/* LOGO */}
-          <div className="flex w-9 items-center justify-center md:w-12">
-            <Image src="/static/jata_logo.png" width={128} height={96} />
+        <Link href={`/${locale === defaultLocale ? "" : locale}`}>
+          <div className="flex cursor-pointer items-center justify-center gap-4">
+            {/* LOGO */}
+            <div className="flex w-9 items-center justify-center md:w-12">
+              <Image src="/static/jata_logo.png" width={128} height={96} />
+            </div>
+            {/* SITE NAME */}
+            <h1 className="text-lg font-semibold uppercase md:text-xl">
+              Kawasanku
+            </h1>
           </div>
-          {/* SITE NAME */}
-          <h1 className="text-lg font-semibold uppercase md:text-xl">
-            Kawasanku
-          </h1>
-        </div>
+        </Link>
         {/* LANGUAGE PICKER */}
         <div className="grid grid-cols-4 divide-x divide-white overflow-hidden rounded-lg border border-white">
           {LANG.map((lang, index) => (
