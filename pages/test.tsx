b1705e3b1ff9375c0883c21b3be90fa3849c6021
chris0610 <chrislow360@gmail.com>
2022-05-26 15:13:13 +0800

    use dynamic import for doughnut chart

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+const TestIfEmbedWorks = () => {
+  return (
+    <iframe
+      src="https://628f275b44db8c0009b7fc97--jovial-sherbet-8a93b1.netlify.app/embed/test"
+      className="min-h-screen w-full"
+    />
+  );
+};
+
+export default TestIfEmbedWorks;
