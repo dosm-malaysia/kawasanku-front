3b87c74f9e0c83b046be3a2224ff1f600942d326
chris0610 <chrislow360@gmail.com>
2022-06-03 17:47:42 +0800

    update tooltip icon

--- components/Charts/Jitterplot/Tooltip.tsx
+++ components/Charts/Jitterplot/Tooltip.tsx
@@ -6,15 +6,20 @@ interface TooltipProps {
 
 const Tooltip: FunctionComponent<TooltipProps> = ({ children }) => {
   return (
-    <div className="tooltip">
+    <div className="tooltip text-gray-400">
       <svg
-        fill="#13293d"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 30 30"
-        width="16px"
-        height="16px"
+        className="h-4 w-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
       >
-        <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M16,21h-2v-7h2V21z M15,11.5 c-0.828,0-1.5-0.672-1.5-1.5s0.672-1.5,1.5-1.5s1.5,0.672,1.5,1.5S15.828,11.5,15,11.5z" />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
       </svg>
 
       <div className="tooltip-content">{children}</div>
