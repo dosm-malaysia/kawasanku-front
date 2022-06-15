ecebc54d4ec69de27c98a1cbec8b2bab9ee2cc4e
chris0610 <chrislow360@gmail.com>
2022-06-15 19:51:13 +0800

    refactor choropleth config and features

--- components/Charts/Choropleth/index.tsx
+++ components/Charts/Choropleth/index.tsx
@@ -11,16 +11,24 @@ import {
   GEO_FILTER,
 } from "../../../lib/constants";
 
-import dunGeojson_mobile from "../../../geojson/dun_mobile.json";
-import parlimenGeojson_mobile from "../../../geojson/parlimen_mobile.json";
-import dunGeojson_desktop from "../../../geojson/dun_desktop.json";
-import parlimenGeojson_desktop from "../../../geojson/parlimen_desktop.json";
+import dunGeojsonMobile from "../../../geojson/dun_mobile.json";
+import dunGeojsonDesktop from "../../../geojson/dun_desktop.json";
+import parlimenGeojsonMobile from "../../../geojson/parlimen_mobile.json";
+import parlimenGeojsonDesktop from "../../../geojson/parlimen_desktop.json";
 
 interface ChoroplethChartProps {
   metric?: CHOROPLETH_METRICS;
   geoFilter?: GEO_FILTER.Parliament | GEO_FILTER.Dun;
   data: IChoroplethData[];
 }
+
+type ChoroplethConfigType = {
+  colors: string[];
+  projectionScale: number;
+  projectionTranslation: [number, number];
+  borderWidth: number;
+  borderColor: string;
+};
 
 const ChoroplethChart: FunctionComponent<ChoroplethChartProps> = ({
   metric,
@@ -29,27 +37,44 @@ const ChoroplethChart: FunctionComponent<ChoroplethChartProps> = ({
 }) => {
   const width = useWindowWidth();
   const isMobile = width < BREAKPOINTS.SM;
+  const isTablet = width <= BREAKPOINTS.MD;
+
+  const choroplethConfig: ChoroplethConfigType = {
+    colors: metric ? getChoroplethColors(metric) : ["#FFF"],
+    projectionScale: isMobile ? 1750 : 3000,
+    projectionTranslation: [
+      isMobile ? 0.5 : isTablet ? 0.52 : 0.65,
+      isMobile ? 0.97 : isTablet ? 1.05 : 0.85,
+    ],
+    borderWidth: metric ? 0.1 : 0.25,
+    borderColor: metric ? getChoroplethBorderColor(metric) : "#13293d",
+  };
+
+  const getChoroplethFeatures = () => {
+    if (isTablet) {
+      if (geoFilter === GEO_FILTER.Dun) return dunGeojsonMobile.features;
+      else return parlimenGeojsonMobile.features;
+    } else {
+      if (geoFilter === GEO_FILTER.Dun) return dunGeojsonDesktop.features;
+      else return parlimenGeojsonDesktop.features;
+    }
+  };
 
   return (
     <div className="h-[388px] sm:h-[588px]">
       <ResponsiveChoropleth
         data={data}
-        features={
-          geoFilter === GEO_FILTER.Dun && isMobile ? dunGeojson_mobile.features :
-          geoFilter === GEO_FILTER.Dun ? dunGeojson_desktop.features :
-          isMobile ? parlimenGeojson_mobile.features : 
-          parlimenGeojson_desktop.features
-        }
+        features={getChoroplethFeatures()}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors={metric ? getChoroplethColors(metric) : ["#FFF"]}
+        colors={choroplethConfig.colors}
         domain={[0, 100]}
         unknownColor="#fff"
         valueFormat=".2s"
-        projectionScale={isMobile ? 1750 : 3000}
-        projectionTranslation={[isMobile ? 0.5 : 0.63, isMobile ? 0.97 : 0.85]}
+        projectionScale={choroplethConfig.projectionScale}
+        projectionTranslation={choroplethConfig.projectionTranslation}
         projectionRotation={[-114, 0, 0]}
-        borderWidth={metric ? 0.1 : 0.25}
-        borderColor={metric ? getChoroplethBorderColor(metric) : "#13293d"}
+        borderWidth={choroplethConfig.borderWidth}
+        borderColor={choroplethConfig.borderColor}
         tooltip={({ feature: { data } }) => {
           return data?.id ? (
             <div className="nivo-tooltip">{data.id}</div>
