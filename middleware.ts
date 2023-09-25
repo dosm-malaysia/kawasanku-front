d2cd25cfa166aa5abffba220ad34a934f5ef69c1
Department of Statistics Malaysia <105629152+dosm-malaysia@users.noreply.github.com>
2023-09-25 15:50:49 +0800

    Hotfix: Update middleware.ts
    Match new OpenDOSM nomenclature

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function middleware() {
-  const url = "https://open.dosm.gov.my/kawasanku";
+  const url = "https://open.dosm.gov.my/dashboard/kawasanku";
   return NextResponse.redirect(url);
 }
